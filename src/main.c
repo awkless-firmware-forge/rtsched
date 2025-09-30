ea23eec26a36951853b5882c4f48dfb7802b116d
Jason Pena <jasonpena@awkless.com>
2025-09-30 13:01:59 -0600

    feat(main): add smoke driver for USART API
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/main.c
+++ src/main.c
@@ -2,28 +2,20 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <avr/interrupt.h>
 #include <avr/io.h>
+#include <util/delay.h>
 
-void
-usart_init(void)
-{
-	UCSR0B = (1 << TXEN0);
-	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-	UBRR0L = 103;
-}
-
-void
-usart_putchar(unsigned char ch)
-{
-	while (!(UCSR0A & (1 << UDRE0)));
-	UDR0 = ch;
-}
+#include "serial.h"
 
 int
 main(void)
 {
-	usart_init();
-	while (1)
-		usart_putchar('A');
+	sei();
+	serial_init(9600);
+	while (1) {
+		serial_puts("Hello world! My name is Jason!\n");
+	}
+
 	return 0;
 }
